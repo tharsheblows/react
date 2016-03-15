71ef6ae58bc48ffe303c7680379c2ebf8f3b4fe6
JJ <jjjay@me.com>
2016-03-15 14:14:51 +0000

    don't include reactions in comment template
    hitting some walls in core so not quite what I’d like

--- lib/class-react.php
+++ lib/class-react.php
@@ -30,6 +30,10 @@ class React {
 		add_action( 'wp_footer',     array( $this,      'print_selector'  ) );
 
  		add_filter( 'the_content',   array( $this,      'the_content'     ) );
+ 		add_filter( 'wp_list_comments_args', array( $this, 'filter_comments' ), 10, 1 );
+ 		add_filter( 'get_comments_number', array( $this, 'count_comments' ), 10, 2 );
+
+ 		// can't use 'parse_comment_query' because it changes all comments queries oh
 	}
 
 	/**
@@ -123,6 +127,83 @@ class React {
 		}
 		$content .= '</div>';
 		return $content;
+	}
+
+	public function filter_comments( $r ){
+
+		//I think this is all we can do? It's all or just one:/src/wp-includes/comment-template.php#L1865
+		$r['type'] = 'comment'; // @todo core -- make this an array or something, needs a filter in the comment-template.php
+
+		return $r;
+
+	}
+
+	// this returns the approved count of comment_type 'comment' -- this needs to be thought through and filtered whatever but for now we're limited by 'wp_list_comment_args' only taking a string
+	// ooh but I can use get_comment_types so why not, this is probably not the right way but I made soooo 
+	public function count_comments( $count, $post_id ){
+		$comment_types = $this->get_comment_types( $post_id );
+		return $comment_types['comment']['approve'];
+	}
+
+	// I don't think there's something like this in core? Or I can't find it, hmmm, that could easily be the case.
+	// This returns a list of comment types in a particular post. We need it to filter wp_list_comments because
+	// there are some we don't want shown. It would be nice to have type__not_in in wp_list_comments but this also
+	// might be a nice thing too. Let's return the count of each type too for extensibility purposes like auto-moderation.
+	// Also I am not certain on best caching practices so this isn't. 
+	// 
+	// Also also this should return approved and unapproved and all that, I haven't really thought it through
+	// Oh fine, let's add in comment statuses too fine fine
+	// 
+	// this is kind of like get_comment_count but with types
+	// 
+	// this is what I'd use for moderation maybe (comment_type = 'moderation_check') - note to self: moderation check is like report this post, it'd replace it
+	public function get_comment_types( $post_id = 0 ){
+		
+		global $wpdb;
+
+		$post_id = ( $post_id === 0 ) ? get_the_ID() : (int)$post_id;
+		if( get_post_status( $post_id ) === false ){
+			return new WP_Error( 'rest_no_post', __( 'Sorry, that post does not exist.', 'react' ) ); 
+		}
+
+		// be nice to have an index on comment_post_id, comment_type, comment_approved maybe
+		$comment_types_array = $wpdb->get_results(
+			$wpdb->prepare(
+				"
+				SELECT comment_type as type, count(*) as count, comment_approved as approved 
+				FROM $wpdb->comments 
+				WHERE comment_post_id = %d
+				GROUP BY comment_type, comment_approved
+				",
+				$post_id
+				)
+			);
+
+		if( !is_array( $comment_types_array ) ){
+			return false;
+		}
+
+		// @todo core hmmm get_comment_statuses needs a filter to add in additional statuses
+		// no wait, get_comment_count uses different words for statuses src/wp-includes/comment.php#L338
+		// let's use 'hold', 'approve' -- not sure on the other ones, so they go to default
+
+		foreach( $comment_types_array as $comment_type_data ){
+			$comment_type_data->type = ( empty( $comment_type_data->type ) ) ? 'comment' : $comment_type_data->type;
+			switch( $comment_type_data->approved ){
+				case 0:
+					$comment_status = 'hold';
+					break;
+				case 1:
+					$comment_status = 'approve';
+					break;
+				default:
+					$comment_status = $comment_type_data->approved;
+			}
+
+			$comment_types[$comment_type_data->type][$comment_status] = $comment_type_data->count;
+		}
+
+		return $comment_types;
 	}
 
 	public function print_selector() {
